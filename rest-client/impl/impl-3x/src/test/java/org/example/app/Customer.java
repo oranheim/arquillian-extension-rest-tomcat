6f88329e33f4beb6340412c3496d6b3d84a931af
oranheim <oranheim@gmail.com>
2014-02-26 16:01:48 +0100

    first commit

---
+++ rest-client/impl/impl-3x/src/test/java/org/example/app/Customer.java
@@ -0,0 +1,28 @@
+package org.example.app;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+/**
+ * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
+ * @version $Revision: 1 $
+ */
+@XmlRootElement
+public class Customer implements Serializable {
+    private String name;
+
+    public Customer() {
+    }
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ rest-client/impl/impl-3x/src/test/java/org/example/app/CustomerResourceImpl.java
@@ -0,0 +1,31 @@
+package org.example.app;
+
+import javax.ws.rs.PathParam;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ora on 26.02.14.
+ */
+public class CustomerResourceImpl implements CustomerResource {
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return new ArrayList<Customer>();
+    }
+
+    @Override
+    public Customer getByName(@PathParam("id")String name) {
+        return new Customer(name);
+    }
+
+    @Override
+    public Customer getById(@PathParam("id") String id) {
+        return new Customer("Bill");
+    }
+
+    @Override
+    public Customer createCustomer(Customer customer) {
+        return new Customer("foo");
+    }
+}
