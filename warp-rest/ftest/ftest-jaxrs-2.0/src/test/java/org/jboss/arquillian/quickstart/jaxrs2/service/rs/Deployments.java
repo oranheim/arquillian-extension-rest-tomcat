d23bae45b15653165818c43e010db69eb326f3f9
oranheim <oranheim@gmail.com>
2014-02-28 07:59:47 +0100

    Minor

--- warp-rest/ftest/ftest-jaxrs-2.0/src/test/java/org/jboss/arquillian/quickstart/jaxrs2/service/rs/Deployments.java
+++ warp-rest/ftest/ftest-jaxrs-2.0/src/test/java/org/jboss/arquillian/quickstart/jaxrs2/service/rs/Deployments.java
@@ -22,11 +22,15 @@ import org.jboss.arquillian.quickstart.jaxrs2.model.Stock;
 import org.jboss.arquillian.quickstart.jaxrs2.service.StockService;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
-import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.PomEquippedResolveStage;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * An utility class that creates the test deployments.
@@ -35,33 +39,28 @@ import java.io.File;
  */
 final class Deployments {
 
+    public static final Collection<JavaArchive> resolveDependencies() {
+        try {
+            PomEquippedResolveStage resolver = Maven.configureResolverViaPlugin();
+            return Arrays.asList(resolver.importCompileAndRuntimeDependencies().resolve().withTransitivity().as(JavaArchive.class));
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
     /**
      * Creates test deployment.
      *
      * @return the test deployment
      */
     public static Archive createDeployment() {
-        File[] libs = loadLibraries();
-
-        return ShrinkWrap.create(WebArchive.class)
+        return ShrinkWrap.create(WebArchive.class, "rest.war")
                 .addClasses(StockApplication.class, Stock.class, StockService.class, StockServiceResource.class)
                 .addAsWebInfResource("WEB-INF/web.xml")
                 .addAsWebResource("restclient.jsp")
                 .addAsWebResource("js/jquery-1.8.2.min.js", "js/jquery-1.8.2.min.js")
-                .addAsLibraries(libs);
+                .addAsLibraries(resolveDependencies());
     }
 
-    /**
-     * Loads all required dependencies needed to run the application in the application server.
-     *
-     * @return the loaded dependencies
-     */
-    private static File[] loadLibraries() {
-        return DependencyResolvers.use(MavenDependencyResolver.class)
-                .artifacts("org.jboss.resteasy:resteasy-jaxrs:3.0.4.Final")
-                .artifacts("org.jboss.resteasy:resteasy-jaxb-provider:3.0.4.Final")
-//                .artifacts("org.jboss.resteasy:resteasy-jackson-provider:3.0.4.Final")
-                .artifacts("org.easytesting:fest-assert:1.4")
-                .resolveAsFiles();
-    }
 }
